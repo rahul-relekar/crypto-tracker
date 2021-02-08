4ab9a4dbb59e446844b1cc2dc20eb185700c54ab
Rahul Relekar <rrelekar2-c@my.cityu.edu.hk>
2021-02-08 16:40:19 +0800

    v1 deployment

--- price-check/src/App.js
+++ price-check/src/App.js
@@ -2,10 +2,25 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 import Coin from "./Coin";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    backgroundColor: "transparent",
+    color:"white",
+    width: "900px",
+  },
+}));
+
 
 function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
+  const classes = useStyles();
 
   useEffect(() => {
     axios
@@ -34,7 +49,7 @@ function App() {
       </div>
       <div className="coin-search">
         {/* Search bar */}
-        <h1 className="coin-text"> Search a currency</h1>
+        <h1 className="coin-text"> Search your crypto</h1>
         <form>
           <input
             type="text"
@@ -45,10 +60,26 @@ function App() {
         </form>
       </div>
 
-      <div className="heading">
-
-        Name | Symbol | Price | Volume | Price Change | Market Cap
-      </div>
+      {/* <Grid container spacing={3}>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>NAME</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>SYMBOL</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>PRICE</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>VOLUME</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>CHANGE</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>MARKET CAP</Paper>
+        </Grid>
+      </Grid> */}
 
       {/* API display from gecko */}
       {filteredCoins.map((coin) => {
--- price-check/src/Coin.js
+++ price-check/src/Coin.js
@@ -1,5 +1,17 @@
 import React from "react";
 import "./Coin.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        backgroundColor: "transparent",
+        color:"white"
+      },
+}));
 
 const Coin = ({
   name,
@@ -10,14 +22,37 @@ const Coin = ({
   priceChange,
   marketcap,
 }) => {
+  const classes = useStyles();
   return (
     <div className="coin-container">
+      {/* <Grid container spacing={3}>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>{name}</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>{symbol}</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>{price}</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>{volume.toLocaleString()}</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>CHANGE</Paper>
+        </Grid>
+        <Grid item xs={2}>
+          <Paper className={classes.paper}>HKD {marketcap.toLocaleString()}</Paper>
+        </Grid>
+      </Grid> */}
+
       <div className="coin-row">
         <div className="coin">
           <img src={image} alt="crypto" />
           <h1>{name}</h1>
           <p className="coin-symbol">{symbol}</p>
         </div>
+
         <div className="coin-data">
           <p className="coin-price">HKD {price}</p>
           <p className="coin-volume">HKD {volume.toLocaleString()}</p>
@@ -27,9 +62,7 @@ const Coin = ({
             <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
           )}
 
-          <p className="coin-marketcap">
-            Mkt Cap:HKD {marketcap.toLocaleString()}
-          </p>
+          <p className="coin-marketcap">HKD {marketcap.toLocaleString()}</p>
         </div>
       </div>
     </div>
